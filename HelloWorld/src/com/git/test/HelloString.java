e4af604c110e194fa251218cfdeaad6d4ac61ec0
dasom <blackless311@naver.com>
2015-09-14 09:27:17 +0900

    hotfix branch change a code

--- HelloWorld/src/com/git/test/HelloString.java
+++ HelloWorld/src/com/git/test/HelloString.java
@@ -5,6 +5,10 @@ public class HelloString {
 	public static void main(String[] args) {
 		String str = "안녕하세요";
 		System.out.println(str);
+		String space = "공간입니다.";
+		System.out.println(space);
+		String job ="programer";
+		System.out.println(job);
 
 	}
 
