12570148cc146fc5845a539faf4f6a7cc364a31e
dasom <blackless311@naver.com>
2015-09-14 12:23:01 +0900

    to String 메소드 재 선언

--- src/com/git/test/PrintFood.java
+++ src/com/git/test/PrintFood.java
@@ -1,13 +1,15 @@
 package com.git.test;
 
-public class PrintFood extends varientFood {
+public class PrintFood extends VarientFood {
 
 	public static void main(String[] args) {
 		Food  food = new Food();
-		varientFood  vf = new varientFood();
+		VarientFood  vf = new VarientFood();
 		
 		System.out.println(food.toString());//  food클래스 toStrion()재선언출력함
 		System.out.println(vf.chnfood); // 상속받은 food 중식이 출력
+		System.out.println(vf.itafood);// 상속받은 food 양식이 출력
+		System.out.println(vf.toString()); 
 		
 		
 
--- src/com/git/test/VarientFood.java
+++ src/com/git/test/VarientFood.java
@@ -1,18 +1,18 @@
 package com.git.test;
 
-public class varientFood extends Food {
+public class VarientFood extends Food {
 
 	String krfood = "비빔밥";
 	String chfood ="짜장면";
 	String itfood = "스파게티";
 	
-	public varientFood(){ // 생성자
+	public VarientFood(){ // 생성자
 		this.krfood=krfood;
 		this.chfood=chfood;
 		this.itafood=itafood;
 	}
 	
-	public varientFood(String krff, String chff, String itff){ // 메소드 오버로딩
+	public VarientFood(String krff, String chff, String itff){ // 메소드 오버로딩
 		super(); // 수퍼메소드 호출
 		this.krfood=krff;
 		this.chfood=chff;
