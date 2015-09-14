a62eea48aaa9970053f2466cd92d148192d776d3
dasom <blackless311@naver.com>
2015-09-14 09:12:30 +0900

    재 시도

---
+++ HelloWorld/src/com/git/test/HelloNumber.java
@@ -0,0 +1,23 @@
+package com.git.test;
+
+public class HelloNumber {
+	public static  void main(String []args){
+		int num1 = 10;//10
+		int num2 = 20;//20
+		 
+		int sum =0; //합계변수 초기화
+		int ca =0;
+
+		int mul=0; 
+
+		sum = num1 +num2; //합계 
+
+		mul = num1*num2;//곱
+
+		System.out.println(sum); // sum 출력
+		System.out.println(ca);// 마이너스출력
+
+		System.out.println(mul);
+
+	}
+}
