116cb3dcbd36f2bb37546217592f4842d0c050e1
dasom <blackless311@naver.com>
2015-09-14 10:17:36 +0900

    다시시작

---
+++ src/com/git/test/HelloString.java
@@ -0,0 +1,17 @@
+package com.git.test;
+
+public class HelloString {
+
+	public static void main(String[] args) {
+		String str = "안녕하세요";
+		System.out.println(str);
+		String space = "공간입니다.";
+		System.out.println(space);
+		String job ="programer";
+		System.out.println(job);
+		String door = "열자입니다.";
+		System.out.println(door);
+
+	}
+
+}
