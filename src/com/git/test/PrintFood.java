3c347868da3c480829f4cfb21ab2fc47a0e06650
dasom <blackless311@naver.com>
2015-09-14 11:37:06 +0900

    extends food example

--- src/com/git/test/PrintFood.java
+++ src/com/git/test/PrintFood.java
@@ -6,8 +6,8 @@ public class PrintFood extends varientFood {
 		Food  food = new Food();
 		varientFood  vf = new varientFood();
 		
-		System.out.println(food.toString());
-		System.out.println(vf.toString());
+		System.out.println(food.toString());//  food클래스 toStrion()재선언출력함
+		System.out.println(vf.chnfood); // 상속받은 food 중식이 출력
 		
 		
 
